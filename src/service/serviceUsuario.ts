a5db28a7a681f46796e102234addbde0c06386d8
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-07-20 21:05:16 -0300

    style: Alterando nome do type do login

--- src/service/serviceUsuario.ts
+++ src/service/serviceUsuario.ts
@@ -2,8 +2,8 @@ import Usuario from '../database/models/Usuario';
 import ErroPersonalizado from '../auxiliares/ErroPersonalizado';
 import { criandoToken } from '../auxiliares/token';
 
-type IloginUsuarioService = {email: string, senha: string}
-export const loginUsuarioService = async ({ email, senha }: IloginUsuarioService) => {
+type loginUsuarioType = { email: string, senha: string };
+export const loginUsuarioService = async ({ email, senha }: loginUsuarioType) => {
   const usuario = await Usuario.findOne({ where: { email, senha } });
   if (usuario === null) throw new ErroPersonalizado(400, '"email" ou "senha" incorreta');
   const novoUsuario = {
