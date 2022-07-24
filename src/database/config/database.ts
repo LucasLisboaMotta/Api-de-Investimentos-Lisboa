09fedf9577c3b9348d8ce892148461c8e6ad2f9d
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-07-24 15:45:37 -0300

    feat: adicionando variavel de ambiente

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config';
-import { Options } from 'sequelize';
+import { Options, Dialect } from 'sequelize';
+
+const dialect = process.env.DB_DIALECT as Dialect;
 
 const config: Options = {
   username: process.env.DB_USER,
@@ -7,7 +9,7 @@ const config: Options = {
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  dialect: 'mysql',
+  dialect,
 };
 
 export = config;
